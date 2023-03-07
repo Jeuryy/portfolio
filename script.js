c3944dd51c831ec38a82e3045acb5935ede6b618
Jeury Pierre Dide <elyruej.102004@gmail.com>
2023-03-07 10:01:53 -0400

    Home and partial about updated

--- script.js
+++ script.js
@@ -20,21 +20,3 @@ const responsiveMenu = () => {
     let x = document.getElementById("nav");
     (x.className === "") ? x.className = "responsive": x.className = "";
 }
-
-//Scroll detect to apply animation
-window.onscroll = function() {
-    skillsEffect()
-};
-
-//function that applies animation to the skills bar
-function skillsEffect() {
-    let skills = document.getElementById("skills");
-    let skills_distance = window.innerHeight = skills.getBoundingClientRect().top;
-    if (skills_distance <= 300) {
-        document.getElementById("html").classList.add("bar-progress1");
-        document.getElementById("js").classList.add("bar-progress2");
-        document.getElementById("bd").classList.add("bar-progress3");
-        document.getElementById("cs").classList.add("bar-progress4");
-    }
-
-}
